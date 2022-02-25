27227168cf5293c5ade60c67e4c8dbdd347b1caa
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-02-25 17:39:55 +0545

    first program

---
+++ Basics_Of_CPP/first_program.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout << "What is your Name ??"<<endl;
+    cin >> name;
+    cout << "Hope you have a good day "<<name<<endl;
+}
