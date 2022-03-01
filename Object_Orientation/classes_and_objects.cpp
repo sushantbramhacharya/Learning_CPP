2c62bd2b3d33fa212812c8688e6fa9be9e8685f7
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-03-01 09:03:00 +0545

    Functions/Method in classes

--- Object_Orientation/classes_and_objects.cpp
+++ Object_Orientation/classes_and_objects.cpp
@@ -1,19 +1,32 @@
-#include<iostream>
+#include <iostream>
 using std::string;
 
-class Employee{
-    //attributes - Private by default
+class Employee
+{
+    // attributes - Private by default
 public:
     string Name;
     string Company;
     int Age;
+    void Intro()
+    {
+        std::cout << "Name - " << Name << std::endl;
+        std::cout << "Company - " << Company << std::endl;
+        std::cout << "Age - " << Age << std::endl;
+    }
 };
 int main()
 {
-    //employee1 is object
+    // employee1 is object
     Employee employee1;
-    //setting Values in Object
-    employee1.Name="Sushant";
-    employee1.Company="XYZ";
-    employee1.Age=20;
+    // setting Values in Object
+    employee1.Name = "Sushant";
+    employee1.Company = "XYZ";
+    employee1.Age = 20;
+    employee1.Intro();
+
+    Employee employee2;
+    employee2.Name="Kali";
+    employee2.Company="Microsoft";
+    employee2.Age=46;
 }
