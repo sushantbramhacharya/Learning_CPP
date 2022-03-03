948316dddebaf2047428b53e181bc6092bfde031
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-03-03 17:15:17 +0545

    Range Based for Loop Introduced in C++11

---
+++ Fundamentals_Of_CPP/range_based_for_loop.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int scores[] = {100, 90, 80};
+    for (auto score : scores) // auto is used so it automatically determine data type or can use datatype
+    {
+        cout << score << endl;
+        // values are score
+    }
+    for (auto item : {50, 60, 30}) // iterate with the array
+    {
+        cout << item << endl;
+        // values are item
+    }
+    for (auto letter : "Sushant") // iterate through string and gives letter
+    {
+        cout << letter << endl;
+        // characters are letter
+    }
+    return 0;
+}
