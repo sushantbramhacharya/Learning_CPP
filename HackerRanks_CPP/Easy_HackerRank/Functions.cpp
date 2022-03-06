4e2f2c334e3c4c97eb12d5702925d44a7d00533b
Guilherme Capucci <gscapucci@hotmail.com>
2022-03-06 13:02:06 -0300

    Use c++ lib

--- HackerRanks_CPP/Easy_HackerRank/Functions.cpp
+++ HackerRanks_CPP/Easy_HackerRank/Functions.cpp
@@ -1,16 +1,13 @@
 //Greatest among 3 numebrs
 #include <iostream>
-#include <cstdio>
-using namespace std;
-
 
 int max_of_four(int a, int b, int c, int d)
 {
-    if(a>b&&a>c&&a>d)
+    if(a > b && a > c && a > d)
         return a;
-    else if(b>a&&b>c&&b>d)
+    else if(b > a && b > c && b > d)
         return b;
-    else if(c>a&&c>b&&c>d)
+    else if(c > a && c > b && c > d)
         return c;
     else 
         return d;
@@ -19,9 +16,7 @@ int max_of_four(int a, int b, int c, int d)
 
 int main() {
     int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
+    std::cin >> a >> b >> c >> d; // is the same scanf("%d %d %d %d", &a, &b, &c, &d);
     int ans = max_of_four(a, b, c, d);
-    printf("%d", ans);
-  
-    return 0;
+    std::cout << ans; // is the same printf("%d", ans);
 }
